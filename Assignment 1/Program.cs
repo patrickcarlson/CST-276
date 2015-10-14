b26e5e9d0610b61d7c6346695ae4aa2a1f8b74c8
Patrick Carlson <patrick.carlson@oit.edu>
2015-10-13 22:58:48 -0700

    Initial commit
    Assignment 1 Initial

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A Morg1 = new A();
+
+            Morg1.setLiving();
+
+            Console.WriteLine("Morg1 is ");
+            if ( Morg1.getLiving() )
+                Console.WriteLine("alive!");
+            else
+                Console.WriteLine("dead!");
+        }
+    }
+
+    interface IMorgObservers
+    {
+        void UpdateLocation();
+        void UpdateLiving();
+    }
+
+    abstract class Morg
+    {
+        private bool living;
+        //private double location;
+        //private int direction; //type?
+       // private List<IMorgObservers> observers;
+
+        public Morg(/*double cLocation, int cDirection*/)
+        {
+            living = true;
+           // location = cLocation;
+            //direction = cDirection;
+        }
+        public bool getLiving() { return living;  }
+
+        public void setLiving()
+        {
+            if (living == true)
+                living = false;
+            else
+                living = true;
+        }
+
+        public void registerObserver(IMorgObservers o)
+        {
+
+        }
+    }
+
+    class A : Morg
+    {
+
+    }
+
+    class B : Morg
+    {
+
+    }
+
+    class C : Morg
+    {
+
+    }
+    class Simulator 
+    {
+
+    }
+
+    class Dish
+    {
+
+    }
+}
